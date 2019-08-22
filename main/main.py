e342c5b7b5a35a1d1d32aefdb60296fb861e81ae
cugiki <diegocugiki2201@gmail.com>
2019-08-21 21:28:41 -0300

    Api principal iniciando uso com requests

--- main/main.py
+++ main/main.py
@@ -1,15 +1,31 @@
-from aiohttp import web
+import requests
+import uvicorn
 
-async def efetua_login():
-    print('blabla')
-    data = {
-        'usuario': 'mestre',
-        'senha' : 'mestra'
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post('/base_a_efetua_login')
+def base_a_efeua_login():
+    url = "http://localhost:8000/login"
+    headers = {
+        'Content-Type': 'application/json'
     }
-    response = web.post('https://localhost:8000')
-    data = response.json()
-    print(data)
-    return data
+    dados = {
+        'usuario': 'mestre',
+        'senha': 'mestra'
+    }
+    requisicao = requests.get(url, headers=headers, json=dados)
+    return requisicao.json()
 
-app = web.Application()
-app.router.add_post('/login', efetua_login())
+if __name__ == '__main__':
+    uvicorn.run(
+        app,
+        host='localhost',
+        port=8003,
+        debug=True,
+        log_level='info',
+        access_log=True,
+        workers=1,
+        timeout_keep_alive=50
+    )
